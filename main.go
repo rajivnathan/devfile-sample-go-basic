b2f29836e560603db79248609e23fa428b961170
Rajiv Senthilnathan <rajivnathan@gmail.com>
2022-07-11 14:39:21 -0400

    Fix url

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const svcName = "devfile-sample-python-basic-git"
 const endpoint = "rajiv"
 
 var port = flag.Int("p", 8080, "server port")
-var url = fmt.Sprintf("http://%s.%s/%s", namespace, svcName, endpoint)
+var url = fmt.Sprintf("http://%s.%s/%s", svcName, namespace, endpoint)
 
 var httpClient = newHTTPClient()
 
