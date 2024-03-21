cb00e9e6c6260c96257e11d7f9565818d4543cd4
Rajiv Senthilnathan <rsenthil@redhat.com>
2024-03-20 21:21:27 -0400

    update

--- main.go
+++ main.go
@@ -19,7 +19,11 @@ func main() {
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[1:]
 	if path != "" {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		resp, err := http.Get("http://pagerduty.com/")
+		if err != nil {
+			fmt.Fprintf(w, "There was an error pinging pagerduty!")
+		}
+		fmt.Fprintf(w, "Resposne Status: %s", resp.Status)
 	} else {
 		fmt.Fprint(w, "Hello World!")
 	}
